a4204f22e816b81afb709d21e5521a99d03dcf30
ZeronSec <ZeronSec@DESKTOP-BCF229R>
2021-03-27 15:16:40 +0530

    commit

---
+++ githab-2/src/main/java/com/git/Githab2Application.java
@@ -0,0 +1,13 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Githab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Githab2Application.class, args);
+	}
+
+}
