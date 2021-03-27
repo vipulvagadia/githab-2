bb519bea56dc6d6e9149f0b0491fd1fbf1734721
vipulvagadia <v2printronix@gmail.com>
2021-03-27 16:11:59 +0530

    update 2

--- githab-2/src/main/java/com/git/Test.java
+++ githab-2/src/main/java/com/git/Test.java
@@ -3,6 +3,11 @@ package com.git;
 public class Test {
 	public static void main(String a[]) {
 		System.out.println("------------");
+		terst();
+	}
+	public static void terst() {
+		
+		System.out.println("+++++++++++++++++");
 	}
 
 }
